3be81037366074a045593fbd8306d4d3d05c320e
ironpxs <joel.choez@hotmail.com>
2021-01-09 18:45:30 -0500

    Paquete vista Agregada vista de ranking (RankingVista) Agregada vista para ingresar el nombre que se escribirá en el ranking (NombreRankingVista)
    y sus controladores.

---
+++ HDragon/src/controlador/NombreRankingVistaControlador.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Administrador
+ */
+public class NombreRankingVistaControlador implements Initializable {
+
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private Button btnEnviarNombre;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ HDragon/src/controlador/RankingVistaControlador.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Administrador
+ */
+public class RankingVistaControlador implements Initializable {
+
+    @FXML
+    private Label lblGanador1;
+    @FXML
+    private Label lblGanador2;
+    @FXML
+    private Label lblGanador3;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
