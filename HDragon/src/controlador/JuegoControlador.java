a992208eb9ef8689d9710b0dbe52bf47fd068e60
ironpxs <joel.choez@hotmail.com>
2021-01-25 19:22:40 -0500

    Organización de imágenes cambiada. Aparición de imágenes para el nivel 1 "Conjunción"

--- HDragon/src/controlador/JuegoControlador.java
+++ HDragon/src/controlador/JuegoControlador.java
@@ -14,7 +14,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import modelo.RutasImagenesNiveles;
 
 
 
@@ -81,8 +83,57 @@ public class JuegoControlador implements Initializable {
             }
         };
         t.schedule(task, 1000,1000);
+        RutasImagenesNiveles nivelConjuncion = new RutasImagenesNiveles();
+        //Enunciado
+        nivelConjuncion.setOperador("/conjuncion/conjuncion.png");
+        nivelConjuncion.setComidaIzquierda("/conjuncion/bebida.png");
+        nivelConjuncion.setComidaDerecha("/conjuncion/canguil.png");
+        //Dragon
+        nivelConjuncion.setDragon("/dragonImg/espera.png");
+        //Opciones
+        nivelConjuncion.setOpcion1("/conjuncion/bebida.png");
+        nivelConjuncion.setOpcion2("/conjuncion/sodayCanguil.png");
+        nivelConjuncion.setOpcion3("/conjuncion/canguil.png");
+        nivelConjuncion.setOpcion4("/conjuncion/hamburguesa.png");
+        colocarImagenes(nivelConjuncion);
         
     }
        
+   private void colocarImagenes(RutasImagenesNiveles rutas){
+       //Enunciado
+       URL linkOperador=getClass().getResource(rutas.getOperador());
+       URL linkComidaIzquierda=getClass().getResource(rutas.getComidaIzquierda());
+       URL linkComidaDerecha=getClass().getResource(rutas.getComidaDerecha());
+       
+       Image imagenOperador = new Image(linkOperador.toString(),47,38,false,true);
+       Image imagenComidaIzquierda = new Image(linkComidaIzquierda.toString(),129,114,false,true);
+       Image imagenComidaDerecha = new Image(linkComidaDerecha.toString(),129,114,false,true);
+       
+       imgOperador.setImage(imagenOperador);
+       imgComida1.setImage(imagenComidaIzquierda);
+       imgComida2.setImage(imagenComidaDerecha);
+       //Dragon
+       URL linkDragon=getClass().getResource(rutas.getDragon());
+       
+       Image imagenDragon = new Image(linkDragon.toString(),394,366,false,true);
+       
+       imgDragon.setImage(imagenDragon);
+       //Opciones
+       URL linkOp1=getClass().getResource(rutas.getOpcion1());
+       URL linkOp2=getClass().getResource(rutas.getOpcion2());
+       URL linkOp3=getClass().getResource(rutas.getOpcion3());
+       URL linkOp4=getClass().getResource(rutas.getOpcion4());
+       
+       Image imagenOp1 = new Image(linkOp1.toString(),129,114,false,true);
+       Image imagenOp2 = new Image(linkOp2.toString(),129,114,false,true);
+       Image imagenOp3 = new Image(linkOp3.toString(),129,114,false,true);
+       Image imagenOp4 = new Image(linkOp4.toString(),129,114,false,true);
+       
+       btnOp1.setGraphic((new ImageView(imagenOp1)));
+       btnOp2.setGraphic((new ImageView(imagenOp2)));
+       btnOp3.setGraphic((new ImageView(imagenOp3)));
+       btnOp4.setGraphic((new ImageView(imagenOp4)));
+        
+   }
     
 }
---
+++ HDragon/src/modelo/RutasImagenesNiveles.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class RutasImagenesNiveles {
+    //Enunciado
+    String operador;
+    String comidaIzquierda;
+    String comidaDerecha;
+    //Dragon
+    String dragon;
+    //Opciones
+    String opcion1;
+    String opcion2;
+    String opcion3;
+    String opcion4;
+
+    public String getOperador() {
+        return operador;
+    }
+
+    public void setOperador(String operador) {
+        this.operador = operador;
+    }
+
+    public String getComidaIzquierda() {
+        return comidaIzquierda;
+    }
+
+    public void setComidaIzquierda(String comidaIzquierda) {
+        this.comidaIzquierda = comidaIzquierda;
+    }
+
+    public String getComidaDerecha() {
+        return comidaDerecha;
+    }
+
+    public void setComidaDerecha(String comidaDerecha) {
+        this.comidaDerecha = comidaDerecha;
+    }
+
+    public String getDragon() {
+        return dragon;
+    }
+
+    public void setDragon(String dragon) {
+        this.dragon = dragon;
+    }
+
+    public String getOpcion1() {
+        return opcion1;
+    }
+
+    public void setOpcion1(String opcion1) {
+        this.opcion1 = opcion1;
+    }
+
+    public String getOpcion2() {
+        return opcion2;
+    }
+
+    public void setOpcion2(String opcion2) {
+        this.opcion2 = opcion2;
+    }
+
+    public String getOpcion3() {
+        return opcion3;
+    }
+
+    public void setOpcion3(String opcion3) {
+        this.opcion3 = opcion3;
+    }
+
+    public String getOpcion4() {
+        return opcion4;
+    }
+
+    public void setOpcion4(String opcion4) {
+        this.opcion4 = opcion4;
+    }
+    
+    
+    
+}
